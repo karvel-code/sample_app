a37473f97ba890748820ba81506b52cc9d343518
karvel-code <velgithinji@gmail.com>
2021-05-25 12:00:59 +0300

    Add test for email prescence and validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,4 +14,9 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
 end
