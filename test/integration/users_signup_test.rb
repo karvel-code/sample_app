b5fc6de2d2f1e7c89dc3ab752718030baf84e7aa
karvel-code <velgithinji@gmail.com>
2021-05-31 12:13:08 +0300

    Add sttyling for error messages

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  class UsersSignupTest < ActionDispatch::IntegrationTest
+
+    test "invalid signup information" do
+      get signup_path
+      assert_no_difference 'User.count' do
+        post users_path, params: { user: { name:  "",
+                                           email: "user@invalid",
+                                           password:              "foo",
+                                           password_confirmation: "bar" } }
+      end
+      assert_template 'users/new'
+      assert_select 'div#error_explanation'
+      assert_select 'div.alert'
+      assert_select 'div.field_with_errors'
+    end
+  end
+end
