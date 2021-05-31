acd937eda06b9ae9ff5b201cdc00e95167a14e4c
karvel-code <velgithinji@gmail.com>
2021-05-31 08:04:37 +0300

    Add private method params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     # debugger
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
