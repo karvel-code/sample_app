5a49327d0aa25c0328101a9c8650ec2873e36016
karvel-code <velgithinji@gmail.com>
2021-06-04 08:01:28 +0300

    Add log in method in sessions helper.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       # Log the user in and redirect to the user's show page.
     else
       # Create an error message.
+      flash.now[:danger] = "Invalid email/password combination"
       render 'new'
     end
   end
