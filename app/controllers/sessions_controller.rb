c22733e6a13adf20dfc7f824a31335fa9bd2425a
karvel-code <velgithinji@gmail.com>
2021-06-09 08:39:34 +0300

    Add methods for rememeber login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
       reset_session
+      remember user
       log_in user
       redirect_to user
     else
-      # Create an error message.
-      flash.now[:danger] = "Invalid email/password combination"
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
