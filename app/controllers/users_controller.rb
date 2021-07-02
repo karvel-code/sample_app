0c1ef2e746d3a327d56efe45a7ea043f08011656
karvel-code <velgithinji@gmail.com>
2021-07-02 11:15:55 +0300

    Add successful test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       #Handle a successful update
+      flash[:success] = "Profile Updated"
+      redirect_to @user
     else
       render 'edit'
     end
